ef424e314ee5d64a3a668f42294f143555348fa4
Ravi M Damor <damorravi540@gmail.com>
2023-12-21 19:56:47 +0530

    Delete src directory

--- src/service/AuthService.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export class AuthService {
-
-    signIn(signInData) {
-        return axios.get('assets/demo/data/loginData.json').then(res => res.data.data.find(user => signInData.username === user.username));
-    }
-
-}
